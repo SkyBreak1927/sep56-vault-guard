78cd12e277de27c2a842c564047fb16e8a8be065
febymarapandebu <febymarapandebu02@gmail.com>
2026-09-16 20:44:45 +0700

    Implement SEP-56 reference vault contract using OpenZeppelin fungible vault module

--- contracts/reference-vault/src/lib.rs
+++ contracts/reference-vault/src/lib.rs
@@ -1,1 +1,43 @@
-// placeholder
+#![no_std]
+
+use soroban_sdk::{contract, contractimpl, Address, Env, MuxedAddress, String};
+use stellar_tokens::{
+    fungible::{Base, FungibleToken},
+    vault::{FungibleVault, Vault},
+};
+
+#[contract]
+pub struct ReferenceVault;
+
+#[contractimpl]
+impl ReferenceVault {
+    pub fn __constructor(
+        e: &Env,
+        name: String,
+        symbol: String,
+        asset: Address,
+        decimals_offset: u32,
+    ) {
+        // Asset and decimal offset should be configured once during
+        // initialization.
+        Vault::set_asset(e, asset);
+        Vault::set_decimals_offset(e, decimals_offset);
+        // Vault overrides the decimals function by default.
+        // Decimal offset must be set prior to metadata initialization.
+        Base::set_metadata(e, Self::decimals(e), name, symbol);
+    }
+}
+
+#[contractimpl(contracttrait)]
+impl FungibleToken for ReferenceVault {
+    type ContractType = Vault;
+
+    // Allows override of decimals and other base functions.
+
+    fn decimals(e: &Env) -> u32 {
+        Vault::decimals(e)
+    }
+}
+
+#[contractimpl(contracttrait)]
+impl FungibleVault for ReferenceVault {}
